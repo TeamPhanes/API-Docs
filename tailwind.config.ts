c92067c0e5048752124dbe06ef9ec9c498df626f
KJongHwa <dblls0724@naver.com>
2025-05-10 18:00:02 +0900

    Chore : 환경 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,29 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        mainBlue: '#2227F7',
+        mainBlueHover: '#060AAC',
+      },
+      width: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      keyframes: {
+        modalIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        modalOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        modalIn: "modalIn 300ms ease-out forwards",
+        modalOut: "modalOut 200ms ease-in forwards",
       },
     },
   },
