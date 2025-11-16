8b1942ec308fa09db2dda52360eccbe4848c1a49
rave189 <rave189@naver.com>
2025-11-16 15:34:48 +0900

    Feature: theme detail api

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -3,7 +3,7 @@ import { DataTypeDTO } from '@/type/DataTypeDTO';
 export const ProjectName = 'Re:Play';
 
 export const DataType: DataTypeDTO = {
-  'O-Auth': {
+  OAuth: {
     GET: {
       '/auth/{socialType}\n소셜 간편 로그인': [
         {
@@ -99,9 +99,9 @@ export const DataType: DataTypeDTO = {
         {
           label: 'Responses',
           content: {
-            totalCount: 'number',
-            currentPage: 'number',
-            data: [
+            totalPages: 'number',
+            number: 'number',
+            content: [
               {
                 id: 'number',
                 title: 'string',
@@ -143,9 +143,9 @@ export const DataType: DataTypeDTO = {
         {
           label: 'Responses',
           content: {
-            totalCount: 'number',
-            currentPage: 'number',
-            data: [
+            totalPages: 'number',
+            number: 'number',
+            content: [
               {
                 id: 'number',
                 title: 'string',
@@ -187,9 +187,9 @@ export const DataType: DataTypeDTO = {
         {
           label: 'Responses',
           content: {
-            totalCount: 'number',
-            currentPage: 'number',
-            data: [
+            totalPages: 'number',
+            number: 'number',
+            content: [
               {
                 id: 'number',
                 title: 'string',
@@ -206,6 +206,203 @@ export const DataType: DataTypeDTO = {
                 avgScore: 'number',
                 isLiked: 'boolean',
                 isVisited: 'boolean',
+              },
+            ],
+          },
+        },
+      ],
+      '/theme/{themeId}\n방탈출 상세 정보 조회': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+        {
+          label: 'Responses',
+          content: {
+            totalPages: 'number',
+            number: 'number',
+            content: [
+              {
+                id: 'number',
+                title: 'string',
+                story: 'string',
+                playtime: 'number',
+                level: 'string',
+                image: 'string',
+                minPlayer: 'number',
+                maxPlayer: 'number',
+                note: 'string',
+                cafeName: 'string',
+                spotName: 'string',
+                address: 'string',
+                phone: 'string',
+                link: 'string',
+                genres: 'string[]',
+                isLiked: 'boolean',
+                isVisited: 'boolean',
+              },
+            ],
+          },
+        },
+      ],
+    },
+    POST: {
+      '/theme/like/{themeId}\n방탈출 좋아요': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+      '/theme/visit/{themeId}\n방탈출 방문': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+    },
+    DELETE: {
+      '/theme/like/{themeId}\n방탈출 좋아요 취소': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+      '/theme/visit/{themeId}\n방탈출 방문 취소': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+    },
+  },
+  Review: {
+    GET: {
+      '/review/{themeId}\n리뷰 목록 조회': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+        {
+          label: 'Responses',
+          content: {
+            totalPages: 'number',
+            number: 'number',
+            content: [
+              {
+                avgScore: 'number',
+                createdGatheringCount: 'number',
+                reviewCount: {
+                  reviewScore: 'number',
+                  count: 'number',
+                },
+                userEvaluation: {
+                  theme: {
+                    label: 'string',
+                    percent: 'number',
+                  },
+                  level: {
+                    label: 'string',
+                    percent: 'number',
+                  },
+                  story: {
+                    label: 'string',
+                    percent: 'number',
+                  },
+                },
+                contents: {
+                  id: 'number',
+                  title: 'string',
+                  story: 'string',
+                  playtime: 'number',
+                  level: 'string',
+                  image: 'string',
+                  minPlayer: 'number',
+                  maxPlayer: 'number',
+                  note: 'string',
+                  cafeName: 'string',
+                  spotName: 'string',
+                  address: 'string',
+                  phone: 'string',
+                  link: 'string',
+                  genres: 'string[]',
+                  isLiked: 'boolean',
+                  isVisited: 'boolean',
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+    POST: {
+      '/review/like/{themeId}\n리뷰 좋아요': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+    },
+    DELETE: {
+      '/review/like/{themeId}\n리뷰 좋아요 취소': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+      ],
+    },
+  },
+  Gathering: {
+    GET: {
+      '/gathering/{themeId}\n생성된 모임 조회': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+        {
+          label: 'Responses',
+          content: {
+            totalPages: 'number',
+            number: 'number',
+            content: [
+              {
+                id: 'number',
+                name: 'string',
+                image: 'string',
+                date: 'date',
+                cafeName: 'string',
+                spotName: 'string',
+                address: 'string',
+                genres: 'string[]',
+                playtime: 'number',
+                level: 'string',
+                isLiked: 'boolean',
               },
             ],
           },
