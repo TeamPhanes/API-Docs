eeae26fe9d9cce87c184a7d41657fc5e3435b10b
rave189 <rave189@naver.com>
2025-11-16 17:03:30 +0900

    Feature: add pagination parameter

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -301,6 +301,13 @@ export const DataType: DataTypeDTO = {
           },
         },
         {
+          label: 'Parameters',
+          content: {
+            page: 'number',
+            size: 'number',
+          },
+        },
+        {
           label: 'Responses',
           content: {
             totalPages: 'number',
