ba324417c9cfeda833ca9a85b2924fdec43a5439
rave189 <rave189@naver.com>
2025-11-17 21:23:49 +0900

    Feature: 모임 목록 조회 및 상세 조회

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -387,7 +387,7 @@ export const DataType: DataTypeDTO = {
   },
   Gathering: {
     GET: {
-      '/gathering/{themeId}\n생성된 모임 조회': [
+      '/gathering\n모임 조회': [
         {
           label: 'Headers',
           content: {
@@ -398,8 +398,102 @@ export const DataType: DataTypeDTO = {
         {
           label: 'Parameters',
           content: {
+            themeId: 'number',
+            locations: 'string[]',
+            genres: 'string[]',
             page: 'number',
             size: 'number',
+          },
+        },
+        {
+          label: 'Responses',
+          content: {
+            totalPages: 'number',
+            number: 'number',
+            content: [
+              {
+                id: 'number',
+                name: 'string',
+                image: 'string',
+                date: 'date',
+                participantCount: 'number',
+                capacity: 'number',
+                title: 'string',
+                address: 'string',
+                genres: 'string[]',
+                playtime: 'number',
+                level: 'string',
+                isLiked: 'boolean',
+              },
+            ],
+          },
+        },
+      ],
+      '/gathering/{gatheringId}\n모임 상세 조회': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
+          },
+        },
+        {
+          label: 'Responses',
+          content: {
+            id: 'number',
+            name: 'string',
+            capacity: 'number',
+            date: 'date',
+            registrationStart: 'date',
+            registrationEnd: 'date',
+            content: 'string',
+            image: 'string',
+            price: 'number',
+            isIndividual: 'boolean',
+            participants: [
+              {
+                id: 'number',
+                profileImage: 'string',
+                nickname: 'string',
+                email: 'string',
+              },
+            ],
+            title: 'string',
+            genres: 'string[]',
+            isLiked: 'boolean',
+            comments: [
+              {
+                id: 'number',
+                userId: 'number',
+                content: 'string',
+                createdAt: 'date',
+                updatedAt: 'date',
+                nickname: 'string',
+                profileImage: 'string',
+                email: 'string',
+                comments: [
+                  {
+                    id: 'number',
+                    userId: 'number',
+                    content: 'string',
+                    createdAt: 'date',
+                    updatedAt: 'date',
+                    nickname: 'string',
+                    profileImage: 'string',
+                    email: 'string',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+      '/gathering/date\n같은 일정 다른 모임 조회': [
+        {
+          label: 'Headers',
+          content: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer {accessToken}',
           },
         },
         {
